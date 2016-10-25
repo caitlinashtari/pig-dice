029eeba46958df94ed9abdbc4f160c4eed544c29
caitlin ashtari and haley swain <caitlin@>
2016-10-24 20:38:56 -0700

    add additional javascript for game functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,37 @@
 /* backend -------------*/
-var die1 = Math.floor(Math.random() * 6) + 1;
-var die2 = Math.floor(Math.random() * 6) + 1;
+var roll1 = Math.floor(Math.random() * 6) + 1;
+var roll2 = Math.floor(Math.random() * 6) + 1;
+
+var totalScore = roll1 + roll2;
+
+var status = function (){
+  if ((roll1 != 1) || (roll2 != 1)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+var totalScore = function (){
+  while(status === true){
+    Player.score += totalScore;
+  }
+}
+
 
 function Player (score, status){
   this.score = score;
   this.status = status;
 }
 
+var player1 = new Player(score, status)
+
+
 
 /* frontend -------------*/
 $(document).ready(function() {
-  $("form").submit(function(event) {
-    event.preventDefault();
+  $("button#click").click(function() {
 
+    totalScore();
 
 
   });
